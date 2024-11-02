b0ce37eeb1c73e74fea371fd64abe015ba0c9629
VladChepurnoj <chepurnojv@gmail.com>
2024-11-02 10:39:36 +0200

    modified custom input fields

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { FormField, FormLabel,FormControl, FormMessage } from './ui/form'
+import { FormField, FormLabel,FormControl, FormMessage, } from './ui/form'
 import { Input } from './ui/input'
 
-import {Control} from 'react-hook-form'
-import { z } from 'zod'
+import { Control, FieldPath } from 'react-hook-form'
+import { z, } from 'zod'
 import { authFormSchema } from '@/lib/utils'
 
 
-
+const formSchema = authFormSchema("sign-up")
 
 interface CustomInput {
-    control: Control<z.infer<typeof authFormSchema>> ,
-    name: string,
+    control: Control<z.infer<typeof formSchema>> ,
+    name: FieldPath<z.infer<typeof formSchema>>,
     label: string,
     placeholder:string
 }
@@ -21,7 +21,7 @@ const CustomInput = ({control, name, label, placeholder}:CustomInput) => {
   return (
     <FormField
     control={control}
-    name="password"
+    name={name}
     render={({ field }) => (
       <div className="form-item">
         <FormLabel className="form-label">
@@ -32,7 +32,7 @@ const CustomInput = ({control, name, label, placeholder}:CustomInput) => {
             <Input
             placeholder='Enter your password'
             className="input-class"
-            type="password"
+            type={name==="password" ? "password":"text"}
             {...field}
            />
           </FormControl>
